3f674c09d6e81b541a557ee02c3f9443d70a106a
Elvy Kiung <kiungh22@gmail.com>
2019-08-15 14:10:22 -0700

    set up database server to run

--- server.js
+++ server.js
@@ -1,30 +1,40 @@
+//dependency
 const express = require("express");
-const path = require("path");
-// const mongoose = require("mongoose");
-// const routes = require("./routes");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
+// modules
+
+const routes = require("./routes");
+
+//set Express connection
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-// app.use(routes);
+// Connect to the Mongo Atlas DB
+const uri = process.env.ATLAS_URI;
 
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/plantsList");
+mongoose.connect(uri || "mongodb://localhost/IndoorPlants", { useNewUrlParser: true, useCreateIndex: true });
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
+
+// Define API routes here
+app.use(routes);
+
+// Server listen
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
