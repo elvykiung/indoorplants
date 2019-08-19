824e208d55ea5f3a85ee0dd742ead207dfd3c4ee
katherine <kmarmitage@gmail.com>
2019-08-18 18:21:30 -0700

    adding client-side routes

---
+++ client/src/utils/API.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export default {
+    // Gets plants from db category (discover)
+    getPlants: function (category) {
+        return axios.get("/api/plants" + category);
+    },
+
+    // Gets plants from db by name (search)
+    getPlantsbyName: function (name) {
+        return axios.get("api/plants" + name);
+    },
+
+    //these routes are not necessary for the MVP but we will use them later:
+
+    // Gets all plants saved to user
+    getMyPlants: function () {
+        return axios.get("/api/myplants");
+    },
+    // Deletes the saved plant with the given id
+    deletePlant: function (id) {
+        return axios.delete("/api/myplants/" + id);
+    },
+    // Saves a plant to the user 
+    savePlant: function (plantData) {
+        return axios.post("/api/myplants", plantData);
+    }
+};
