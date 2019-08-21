65ca6ad2637a399cf963eb7fb5cc39803e952e16
Elvy Kiung <kiungh22@gmail.com>
2019-08-21 10:20:14 -0700

    discove main presentation

--- client/src/pages/DiscoveryMain.js
+++ client/src/pages/DiscoveryMain.js
@@ -5,14 +5,45 @@
 // - Button to call list item page component
 //   Sticky bottom navbar component
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 class DiscoveryMain extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <p>This is Discovery Page</p>
-      </div>
+      <Container>
+        <Jumbotron fluid className="text-center">
+          <h1 className="text-primary">Discovery Your Plants</h1>
+        </Jumbotron>
+        <Card as="a" style={{ margin: "auto" }}>
+          <Card.Img src="https://3c9sm1yzqy518hwx3f6o4c64-wpengine.netdna-ssl.com/wp-content/uploads/2017/02/Succulent-1--1030x381.jpg" alt="Easy to growth Plants" />
+          <Card.ImgOverlay>
+            <Card.Title className="text-white text-center" style={{ "font-size": "6vw", margin: "10%", fontWeight: "bold" }}>
+              Easy to growth Plants
+            </Card.Title>
+          </Card.ImgOverlay>
+        </Card>
+        <br />
+        <Card as="a">
+          <Card.Img src="https://s3-us-west-1.amazonaws.com/landgbucket/images/products/6c0f98f2-5af2-44ab-9f97-ec7fc693b8a0.jpeg?1566068123.70990019800" alt="Decorative Plants" />
+          <Card.ImgOverlay>
+            <Card.Title className="text-danger text-center" style={{ "font-size": "8vw", margin: "20%0px", fontWeight: "bold" }}>
+              Decorative Plants
+            </Card.Title>
+          </Card.ImgOverlay>
+        </Card>
+        <br />
+        <Card as="a" style={{ margin: "auto" }}>
+          <Card.Img src="https://i.pinimg.com/originals/71/6a/39/716a39edf1b3de80260b943982628c49.jpg" alt="Rare Plants" />
+          <Card.ImgOverlay>
+            <Card.Title className="text-info text-center" style={{ "font-size": "8vw", margin: "20%", fontWeight: "bold" }}>
+              Rare Plants
+            </Card.Title>
+          </Card.ImgOverlay>
+        </Card>
+      </Container>
     );
   }
 }
