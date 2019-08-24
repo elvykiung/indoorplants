b25a01152eafb918e82bfb2e8c429cd2f09dd2f7
katherine <kmarmitage@gmail.com>
2019-08-24 11:15:42 -0700

    adding page content

--- client/src/pages/MyPlantsMain.js
+++ client/src/pages/MyPlantsMain.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+
 import API from "../utils/API";
 import ListItems from "../components/ListItems";
-import Container from "react-bootstrap/Container";
-import Jumbotron from "react-bootstrap/Jumbotron";
 
 class MyPlantsMain extends Component {
     //need to set the state to reflect the user's plant info - I think this will depend on how
@@ -11,12 +15,60 @@ class MyPlantsMain extends Component {
         user: []
     };
 
+    componentDidMount() {
+        this.getUserPlants();
+    }
+
+    getUserPlants = () => {
+        API.getUserPlants(userid) //
+            .then(res =>
+                this.setState({
+                    ifResults: true,
+                    user: res.data
+                })
+            )
+            .catch(err => console.log(err));
+    };
+
 
     render() {
         return (
-            <div>
-                <p>This is my plants main page</p>
-            </div>
+            <Container>
+                <Jumbotron fluid className="text-center">
+                    <h1 className="text-primary">Your Plants</h1>
+                </Jumbotron>
+
+                <div>
+                    <div className="col-10 col-centered card-content mb-4">
+                        {this.state.ifResults ? (
+                            <div>
+                                <h1 className="heading-title mx-sm-3 mb-2 text-center">Your Saved Plants</h1>
+
+                                {this.state.results.map(user => {
+                                    return <ListItems
+                                        key={user.plant._id}
+                                        images={"http://www.costafarms.com/CostaFarms/" + user.plant.image}
+                                        commonName={user.plant.commonName}
+                                        scientificName={user.plant.scientificName}
+                                        //description={user.plant.fullDescription}
+                                        title={user.plant.title}
+                                    />
+
+                                })}
+
+                            </div>
+                        ) : (
+                                <div>
+                                </div>
+
+
+                            )
+                        }
+
+                    </div>
+                </div>
+
+            </Container>
         );
     }
 }
