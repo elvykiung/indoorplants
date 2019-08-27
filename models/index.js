2e125a868bd67eb7d6b8b8e8a1a10af0df9a6cf6
katherine <kmarmitage@gmail.com>
2019-08-26 21:13:16 -0700

    added user schema with email validation

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Exporting an object containing all of our models
+
+module.exports = {
+    Plant: require("./plant"),
+    User: require("./user")
+};
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//validate email with regex
+const validateEmail = function (email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    }
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
