2b5d7e7de0452b75ad41cfb54c678f635203f8da
Elvy Kiung <kiungh22@gmail.com>
2019-08-27 20:33:57 -0700

    routing the pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootswatch/dist/minty/bootstrap.min.css";
 import LoginHome from "./pages/LoginHome";
 import Login from "./pages/LogIn";
-// import myPlants from "./pages/MyPlants";
 import SearchMain from "./pages/SearchMain";
 import DiscoveryMain from "./pages/DiscoveryMain";
 import DiscoverListItemsPage from "./pages/DiscoverListItemsPage";
 import DetailPlant from "./pages/DetailPlant";
 import StickyBottom from "./components/Nav";
+import MyPlantsMain from "./pages/MyPlantsMain";
 import MyPlantsDetail from "./pages/MyPlantsDetailPage";
 
 function App() {
@@ -17,9 +17,9 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/home" component={LoginHome} />
-          {/* <Route exact path="/myPlants" component={myPlantsMain} /> */}
+          <Route exact path="/myPlants" component={MyPlantsMain} />
           {/* <Route exact path="/" component={LoginHome} /> */}
-          <Route exact path="/myPlants" component={MyPlantsDetail} />
+          <Route exact path="/myPlants/detail" component={MyPlantsDetail} />
           <Route exact path="/search" component={SearchMain} />
           <Route exact path="/discovery" component={DiscoveryMain} />
           <Route exact path={"/easy-to-grow"} component={() => <DiscoverListItemsPage category={"easy-to-grow"} />} />
--- client/src/components/PlantToDoList/index.js
+++ client/src/components/PlantToDoList/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
-import Button from "react-bootstrap/Button";
+// import ListGroup from "react-bootstrap/ListGroup";
+// import Button from "react-bootstrap/Button";
 
 class UserToDo extends Component {
   state = {};
