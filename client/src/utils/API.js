e76b9dbd93b43b1450f1d030727145f67163f836
katherine <kmarmitage@gmail.com>
2019-08-28 22:23:27 -0700

    modified routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,32 +2,36 @@ import axios from "axios";
 
 export default {
   // Gets plants from db category (discover)
-  getPlants: function(category) {
+  getPlants: function (category) {
     return axios.get("/api/plants/category/" + category);
   },
 
   // Gets plants from db by name (search)
-  getPlantsbyName: function(name) {
+  getPlantsbyName: function (name) {
     return axios.get("api/plants/" + name);
   },
 
   // Gets plant by id
-  getPlantsbyID: function(id) {
+  getPlantsbyID: function (id) {
     return axios.get("api/plants/id/" + id);
   },
 
   //these routes are not necessary for the MVP but we will use them later:
 
   // Gets all plants saved to user
-  getMyPlants: function() {
-    return axios.get("/api/plants");
+  getMyPlants: function (userid) {
+    return axios.get("/api/myplants" + userid);
+  },
+  // Gets plant for detail page
+  getMyPlants: function (plantid) {
+    return axios.get("/api/myplants" + plantid);
   },
   // Deletes the saved plant with the given id
-  deletePlant: function(id) {
-    return axios.delete("/api/plants/" + id);
+  deleteMyPlant: function (id) {
+    return axios.delete("/api/delete/myplants/" + id);
   },
   // Saves a plant to the user
-  savePlant: function(plantData) {
-    return axios.post("/api/plants", plantData);
+  saveMyPlant: function (plantData) {
+    return axios.post("/api/save/myplants", plantData);
   }
 };
