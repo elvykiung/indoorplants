94590b2379fdc76617f8a32d0765e9c70a7e2150
katherine <kmarmitage@gmail.com>
2019-08-29 19:22:07 -0700

    trying to test

--- client/src/pages/MyPlants.js
+++ client/src/pages/MyPlants.js
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 
 class MyPlants extends Component {
   state = {};
+
+  //testing plantCare routes & queries
+
+  createPlant = () => {
+    API.saveMyPlant(plantData)
+      .then(res =>
+        console.log(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+  //  let plantData = { userid: 1234, plantid: 5678 };
+
+
+
+
   render() {
     return (
       <div>
