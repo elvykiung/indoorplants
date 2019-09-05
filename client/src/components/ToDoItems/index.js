2c6636e10b87b4e7574096aab947f25d87f3ce4d
Elvy Kiung <kiungh22@gmail.com>
2019-09-05 13:58:21 -0700

    try new card ui

--- client/src/components/ToDoItems/index.js
+++ client/src/components/ToDoItems/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
+import { TaggedContentCard } from "react-ui-cards";
+import styles from "./styles.css";
 
 function ToDoItems(props) {
   return (
-    <Card style={{ marginBottom: "15px", width: "120%" }} as={Link} to={"/myPlants/detail/" + props.id}>
-      <Card.Img src={props.image} style={{ width: "40%", height: "auto", marginBottom: "10px", marginTop: "10px", marginLeft: "10px" }} alt={props.alt} />
+    <Card style={{ marginBottom: "15px", width: "auto" }} as={Link} to={"/myPlants/detail/" + props.id}>
+      <div className={styles["card-container"]}>
+        <TaggedContentCard thumbnail={props.image} title={props.commonName} description={props.scientificName} tags={["water", "light", "fertilizing"]} />
+      </div>
     </Card>
   );
 }
