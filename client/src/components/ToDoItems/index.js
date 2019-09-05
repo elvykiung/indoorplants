6251c932c1e6980c2b6165012af10bf337d82dd6
Elvy Kiung <kiungh22@gmail.com>
2019-09-05 15:14:02 -0700

    remove card

--- client/src/components/ToDoItems/index.js
+++ client/src/components/ToDoItems/index.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Card from "react-bootstrap/Card";
 import { TaggedContentCard } from "react-ui-cards";
 import styles from "./styles.css";
 
 function ToDoItems(props) {
   return (
-    <Card style={{ marginBottom: "15px", width: "auto" }} as={Link} to={"/myPlants/detail/" + props.id}>
-      <div className={styles["card-container"]}>
+    <div className={styles["card-container"]}>
+      <Link to={"/myPlants/detail/" + props.id}>
         <TaggedContentCard thumbnail={props.image} title={props.commonName} description={props.scientificName} tags={["water", "light", "fertilizing"]} />
-      </div>
-    </Card>
+      </Link>
+    </div>
   );
 }
 
